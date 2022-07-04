993157844c28491e28778b423402d3bb3b3aa5f4
Grzegorz Mikina <gregorimikina@gmail.com>
2022-07-04 23:33:15 +0200

    Create QuoteController, index and contruct

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 use App\Models\Quote;
+use App\Repository\iQuoteRepo;
 use Illuminate\Http\Request;
 
 class QuoteController extends Controller
 {
-    public function index(){
+    private iQuoteRepo $quoteRepo;
 
-        $quotes = Quote::all();
+    public function __construct(iQuoteRepo $quoteRepo)
+    {
+        $this->quoteRepo = $quoteRepo;
+    }
+    public function index(){
+       // $quotes = Quote::all();
+        $quotes = $this->quoteRepo->all();
+
+        return response()->json([
+            'status'=> true,
+            'data'=>$quotes,
+            'message'=>'Query Ok',
+
+        ]);
     }
 }
